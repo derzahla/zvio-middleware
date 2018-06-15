truenas/middleware

commit aa87d9e89a51414ef43b37cac593edb02294a0b8

Merge migrations after merging cloud credentials to stable
Ticket: #29020

gui/system/migrations/0022_cert_serial.py
@@ -9,7 +9,7 @@ import freenasUI.freeadmin.models.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('system', '0021_merge_20180507_1315'),
+        ('system', '0021_merge_20180612_2359'),
     ]
 
     operations = [

gui/system/migrations/0016_encrypt_cloud_credentials.py
@@ -50,7 +50,7 @@ def encrypt_cloud_credentials(apps, schema_editor):
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('system', '0015_alert'),
+        ('system', '0009_auto_20171023_2159'),
     ]
 
     operations = [

gui/system/migrations/0021_merge_20180612_2332.py
@@ -1,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Generated by Django 1.10.8 on 2018-06-12 23:32
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('system', '0016_encrypt_cloud_credentials'),
+        ('system', '0020_auto_20180502_1200'),
+    ]
+
+    operations = [
+    ]

gui/system/migrations/0021_merge_20180612_2359.py
@@ -1,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Generated by Django 1.10.8 on 2018-06-12 23:59
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('system', '0015_alert'),
+        ('system', '0021_merge_20180507_1315'),
+        ('system', '0021_merge_20180612_2332'),
+    ]
+
+    operations = [
+    ]

derzahla/zvio-middleware

commit 1c1cd25168d669fb35be3e26fd46062b94f14472

Fix migrations bug introduced in aa87d9e89a51414ef43b37cac593edb02294a0b8

gui/system/migrations/0022_cert_serial.py
@@ -9,7 +9,7 @@ import freenasUI.freeadmin.models.fields
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('system', '0021_merge_20180612_2359'),
+        ('system', '0021_merge_20180507_1315'),
     ]
 
     operations = [

gui/system/migrations/0023_merge_20180612_2359.py
@@ -9,8 +9,8 @@ class Migration(migrations.Migration):
 
     dependencies = [
         ('system', '0015_alert'),
-        ('system', '0021_merge_20180507_1315'),
         ('system', '0021_merge_20180612_2332'),
+        ('system', '0022_cloud_sync'),
     ]
 
     operations = [