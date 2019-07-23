danny8376/freenas

Add comment field for  initshutdown model
This commit adds a migration and updates the initshutdown model to include a comment field.

gui/tasks/migrations/0014_ini_comment_field.py
@@ -1,0 +1,19 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tasks', '0013_init_tasks_timeout'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='initshutdown',
+            name='ini_comment',
+            field=models.TextField(
+                verbose_name='Comment',
+                blank=True,
+            ),
+        ),
+    ]

derzahla/zvio-middleware

gui/tasks/migrations/0014_ini_comment_field.py
@@ -1,0 +1,20 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('tasks', '0013_init_tasks_timeout'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='initshutdown',
+            name='ini_comment',
+            field=models.CharField(
+                verbose_name='Comment',
+                blank=True,
+                max_length=255,
+            ),
+        ),
+    ]