advid1922/freenas

Expose ciphers/digests
This commit makes sure that valid ciphers/digests are exposed by openvpn services so the user can correctly choose which one to use.

src/middlewared/middlewared/plugins/vpn.py
@@ -107,6 +107,14 @@ class OpenVPNServerService(SystemServiceService):
         service_model = 'openvpnserver'
         service_verb = 'restart'
 
+    @accepts()
+    async def digests(self):
+        return OpenVPN.digests()
+
+    @accepts()
+    async def ciphers(self):
+        return OpenVPN.ciphers()
+
     @private
     async def validate(self, data, schema_name):
         verrors = await OpenVPN.common_validation(
@@ -156,6 +164,14 @@ class OpenVPNClientService(SystemServiceService):
         service = 'openvpn_client'
         service_model = 'openvpnclient'
         service_verb = 'restart'
+
+    @accepts()
+    async def digests(self):
+        return OpenVPN.digests()
+
+    @accepts()
+    async def ciphers(self):
+        return OpenVPN.ciphers()
 
     @private
     async def validate(self, data, schema_name):

derzahla/zvio-middleware

src/middlewared/middlewared/plugins/vpn.py
@@ -101,6 +101,14 @@ class OpenVPNServerService(SystemServiceService):
         service_model = 'openvpnserver'
         service_verb = 'restart'
 
+    @accepts()
+    async def authentication_algorithm_choices(self):
+        return OpenVPN.digests()
+
+    @accepts()
+    async def cipher_choices(self):
+        return OpenVPN.ciphers()
+
     @private
     async def validate(self, data, schema_name):
         verrors = await OpenVPN.common_validation(
@@ -169,6 +177,14 @@ class OpenVPNClientService(SystemServiceService):
         service = 'openvpn_client'
         service_model = 'openvpnclient'
         service_verb = 'restart'
+
+    @accepts()
+    async def authentication_algorithm_choices(self):
+        return OpenVPN.digests()
+
+    @accepts()
+    async def cipher_choices(self):
+        return OpenVPN.ciphers()
 
     @private
     async def validate(self, data, schema_name):