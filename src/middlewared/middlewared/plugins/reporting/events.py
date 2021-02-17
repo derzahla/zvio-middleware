truenas/middleware

commit 3ff60f8fd876cac83538fe6b15b58db7cb0bc17e

reporting.realtime interface speeds
(cherry picked from commit a5bc4f014e0d892facef4aca1ee4c790f2aa77c1)

src/middlewared/middlewared/plugins/reporting/events.py
@@ -1,6 +1,7 @@
 import itertools
 import json
 import psutil
+import re
 import subprocess
 import time
 
@@ -12,12 +13,19 @@ if osc.IS_FREEBSD:
     import netif
 
 
+MEGABIT = 131072
+RE_BASE = re.compile(r'([0-9]+)base')
+RE_MBS = re.compile(r'([0-9]+)Mb/s')
+
+
 class RealtimeEventSource(EventSource):
 
     """
     Retrieve real time statistics for CPU, network,
     virtual memory and zfs arc.
     """
+
+    INTERFACE_SPEEDS_CACHE_INTERLVAL = 300
 
     @staticmethod
     def get_cpu_usages(cp_diff):
@@ -46,11 +54,50 @@ class RealtimeEventSource(EventSource):
             data['usage'] = 0
         return data
 
+    def get_interface_speeds(self):
+        speeds = {}
+
+        interfaces = self.middleware.call_sync('interface.query')
+        for interface in interfaces:
+            if m := RE_BASE.match(interface['state']['active_media_subtype']):
+                speeds[interface['name']] = int(m.group(1)) * MEGABIT
+            elif m := RE_MBS.match(interface['state']['active_media_subtype']):
+                speeds[interface['name']] = int(m.group(1)) * MEGABIT
+
+        types = ['BRIDGE', 'LINK_AGGREGATION', 'VLAN']
+        for interface in sorted([i for i in interfaces if i['type'] in types], key=lambda i: types.index(i['type'])):
+            speed = None
+
+            if interface['type'] == 'BRIDGE':
+                member_speeds = [speeds.get(member) for member in interface['bridge_members'] if speeds.get(member)]
+                if member_speeds:
+                    speed = max(member_speeds)
+
+            if interface['type'] == 'LINK_AGGREGATION':
+                port_speeds = [speeds.get(port) for port in interface['lag_ports'] if speeds.get(port)]
+                if port_speeds:
+                    if interface['lag_protocol'] in ['LACP', 'LOADBALANCE', 'ROUNDROBIN']:
+                        speed = sum(port_speeds)
+                    else:
+                        speed = min(port_speeds)
+
+            if interface['type'] == 'VLAN':
+                speed = speeds.get(interface['vlan_parent_interface'])
+
+            if speed:
+                speeds[interface['name']] = speed
+
+        return speeds
+
     def run(self):
 
         cp_time_last = None
         cp_times_last = None
         last_interface_stats = {}
+        last_interface_speeds = {
+            'time': time.monotonic(),
+            'speeds': self.get_interface_speeds(),
+        }
 
         while not self._cancel.is_set():
             data = {}
@@ -141,11 +188,20 @@ class RealtimeEventSource(EventSource):
                                     data['cpu']['temperature'][core] = value
                                     break
 
+            # Interface related statistics
+            if last_interface_speeds['time'] < time.monotonic() - self.INTERFACE_SPEEDS_CACHE_INTERLVAL:
+                last_interface_speeds.update({
+                    'time': time.monotonic(),
+                    'speeds': self.get_interface_speeds(),
+                })
+            data['interfaces'] = defaultdict(dict)
+            retrieve_stat = {'rx_bytes': 'received_bytes', 'tx_bytes': 'sent_bytes'}
             if osc.IS_FREEBSD:
                 # Interface related statistics
                 data['interfaces'] = {}
                 retrieve_stat_keys = ['received_bytes', 'sent_bytes']
                 for iface in netif.list_interfaces().values():
+                    data['interfaces'][iface.name]['speed'] = last_interface_speeds['speeds'].get(iface.name)
                     for addr in filter(lambda addr: addr.af.name.lower() == 'link', iface.addresses):
                         addr_data = addr.__getstate__(stats=True)
                         stats_time = time.time()
@@ -163,6 +219,7 @@ class RealtimeEventSource(EventSource):
                             }
                             data['interfaces'][iface.name].update(details_dict)
                         last_interface_stats[iface.name] = {**data['interfaces'][iface.name], 'stats_time': stats_time}
+
             self.send_event('ADDED', fields=data)
             time.sleep(2)
 

derzahla/zvio-middleware

commit 41cf5c9e87b908c25f0af75ef5d2b971e7b417d0

Fix reporting.realtime crash introduced in 3ff60f8fd876cac83538fe6b15b58db7cb0bc17e

src/middlewared/middlewared/plugins/reporting/events.py
@@ -211,8 +211,6 @@ class RealtimeEventSource(EventSource):
                     'time': time.monotonic(),
                     'speeds': self.get_interface_speeds(),
                 })
-            data['interfaces'] = defaultdict(dict)
-            retrieve_stat = {'rx_bytes': 'received_bytes', 'tx_bytes': 'sent_bytes'}
             if osc.IS_FREEBSD:
                 # Interface related statistics
                 data['interfaces'] = {}